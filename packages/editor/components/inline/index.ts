cdefce25b1d0c8389d563c64b92427ac99eb967a
Vigram Sivakumar <vigram.work@gmail.com>
2026-01-26 17:36:36 +0530

    chore: remove deprecated MentionPill component
    Replaced by InlineToken + TokenPill primitives. No longer used anywhere in the codebase.

--- packages/editor/components/inline/index.ts
+++ packages/editor/components/inline/index.ts
@@ -2,5 +2,4 @@
 export { DateMentionInline } from './DateMentionInline';
 export { HashtagInline } from './HashtagInline';
 export { NoteLinkInline } from './NoteLinkInline';
-export { MentionPill } from './MentionPill';
 export { InlineNode } from './InlineNode';
