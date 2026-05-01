9366f62fe5a61cf84b55d535d316b8d6d3e07430
Vigram Sivakumar <vigram.work@gmail.com>
2026-05-01 21:08:56 +0530

    feat: cleared old style sheets

--- apps/editor/src/main.tsx
+++ apps/editor/src/main.tsx
@@ -9,7 +9,6 @@ import './design-system/tokens.css';
 import './design-system/theme.css';
 import './styles/base.css';
 import './styles/app-layout.css';
-import './styles/ui.css';
 import './styles/topbar.css';
 import './styles/editor.css';
 
